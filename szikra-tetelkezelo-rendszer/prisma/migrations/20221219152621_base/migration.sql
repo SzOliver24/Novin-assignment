9d949dd38f4d209c1f8916973ae885d0a7aae5d7
Oliver Szikra <oliver.szikra@gmail.com>
2022-12-19 16:30:07 +0100

    created schema for database

---
+++ szikra-tetelkezelo-rendszer/prisma/migrations/20221219152621_base/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(120) NOT NULL,
+    `userName` VARCHAR(120) NOT NULL,
+    `email` VARCHAR(120) NULL,
+    `password` VARCHAR(120) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `role` VARCHAR(120) NOT NULL DEFAULT 'basic',
+
+    UNIQUE INDEX `user_userName_key`(`userName`),
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `customer` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(120) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `item` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(120) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `comment` VARCHAR(255) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `status` VARCHAR(120) NOT NULL,
+    `customerId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `customer` ADD CONSTRAINT `customer_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `item` ADD CONSTRAINT `item_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `customer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
